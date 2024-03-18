6bea3a3171e637c7d7fa07dc733457cb6ad07e8f
Ali <alirabbani54@gmail.com>
2024-03-18 11:31:24 +0500

    created book showing details

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,9 +13,10 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost.com/books")
+      .get("http://localhost:5555/books")
       .then((response) => {
         setBooks(response.data.data);
+        console.log(JSON.stringify(response.data.data));
         setLoading(false);
       })
       .catch((error) => {
@@ -29,9 +30,60 @@ const Home = () => {
       <div className="flex justify-center items-center">
         <h1 className="text-3xl my-8">Book List</h1>
         <Link to="/books/create">
-            <MdOutlineAddBox className="text-sky-800 text-4xl"/>
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
         </Link>
       </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table className="w-full border-separate border-spacing-2">
+          <thead>
+            <tr>
+              <th className="border border-slate-600 rounded-md">No.</th>
+              <th className="border border-slate-600 rounded-md">Title</th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Author
+              </th>
+              <th className="border border-slate-600 rounded-md max-md:hidden">
+                Published Year
+              </th>
+              <th className="border border-slate-600 rounded-md">Operations</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book, index) => (
+              <tr key={book._id} className="h-8">
+                <td className="border border-slate-700 rounded-md text-center">
+                  {index + 1}
+                </td>
+                <td className="border border-slate-600 rounded-md">
+                  {book.title}
+                </td>
+                <td className="border border-slate-600 rounded-md max-md:hidden">
+                  {book.author}
+                </td>
+                <td className="border border-slate-600 rounded-md max-md:hidden">
+                  {book.publishYear}
+                </td>
+                <td className="border border-slate-600 rounded-md">
+                  <div className="flex justify-center gap-x-4 ">
+                    <Link to={`/books/details/${book._id}`}>
+                      <BsInfoCircle className="text-2xl text-green-800" />
+                    </Link>
+
+                    <Link to={`/books/edit/${book._id}`}>
+                      <AiOutlineEdit className="text-2xl text-yellow-600" />
+                    </Link>
+                    <Link to={`/books/delete/${book._id}`}>
+                      <MdOutlineDelete className="text-2xl text-red-600" />
+                    </Link>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Spinner from "../components/Spinner";
+import BackButton from "../components/BackButton";
+import { useParams } from "react-router-dom";
 
 const ShowBook = () => {
-  return (
-    <div>ShowBook</div>
-  )
-}
+  const [book, setBook] = useState({});
+  const [loading, setLoading] = useState(false);
 
-export default ShowBook
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setBook(response.data.data);
+        console.log(response.data)
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+    console.log(book)
+
+  return (
+    <div className="p-4 w-full">
+      <BackButton />
+      <h1 className="text-3xl my-4">Show Book</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className=" flex flex-col border-[4px] rounded-2xl border-spacing-2 p-4 mx-auto border-sky-500 w-fit ">
+          <div className="my-4">
+            <span className="text-2xl font-semibold text-gray-500 mr-4">Id</span>
+            <span className="text-2xl text-gray-500 mx-5">{book._id}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-2xl font-semibold text-gray-500 mr-4">Title</span>
+            <span className="text-2xl text-gray-500 mx-5">{book.title}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-2xl font-semibold text-gray-500 mr-4">Author</span>
+            <span className="text-2xl text-gray-500 mx-5">{book.author}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-2xl font-semibold text-gray-500 mr-4">Publish Year</span>
+            <span className="text-2xl text-gray-500 mx-5">{book.publishYear}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-2xl font-semibold text-gray-500 mr-4">Created At</span>
+            <span className="text-2xl text-gray-500 mx-5">{new Date(book.createdAt).toString()}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-2xl font-semibold text-gray-500 mr-4">Updated At</span>
+            <span className="text-2xl text-gray-500 mx-5">{new Date(book.updatedAt).toString()}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ShowBook;
